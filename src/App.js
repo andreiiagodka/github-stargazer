ca97e839144b1a3fba542aef7ce46574257b69f9
Andrey Yagodka <andrei.iagodka@gmail.com>
2021-01-09 19:23:35 +0200

    Add Home and Details component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home/Home';
+import Details from './components/Details/Details';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello world!</h1>
+      <Router>
+        <Switch>
+          <Route path='/details'>
+            <Details />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ src/components/Details/Details.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Details extends Component {
+  render() {
+    return (
+      <h1>Details page</h1>
+    )
+  }
+}
+
+export default Details
