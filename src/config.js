9306ffb2615dcd67c510dbde95ee156f00ff8734
Andrey Yagodka <andrei.iagodka@gmail.com>
2021-01-12 10:01:39 +0200

    Add firebase

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import Firebase from 'firebase' 
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBEbxms7VS5GHHQe9l7EltA66TWBhaI6Yk',
+  authDomain: 'localhost',
+  databaseURL: 'https://github-stargazer-4e8a3-default-rtdb.europe-west1.firebasedatabase.app/',
+  projectId: 'github-stargazer-4e8a3'
+}
+
+const firebase = Firebase.initializeApp(firebaseConfig)
+
+export default firebase
