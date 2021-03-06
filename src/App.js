9d1e5fa80457e294d7224f24e85790df184e354b
Andrey Yagodka <andrei.iagodka@gmail.com>
2021-03-06 18:31:33 +0200

    Redesign App component as functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Container, Col, Card } from 'react-bootstrap'
@@ -7,34 +7,32 @@ import Home from './containers/Home/Home'
 import Details from './containers/Details/Details'
 import AddRepository from './components/AddRepository/AddRepository'
 
-class App extends Component {
-  render() {
-    let routes = (
-      <Router>
-        <Switch>
-          <Route exact path='/new'>
-            <AddRepository />
-          </Route>
-          <Route exact path='/details/:id'>
-            <Details />
-          </Route>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    )
+const App = () => {
+  const routes = (
+    <Router>
+      <Switch>
+        <Route exact path='/new'>
+          <AddRepository />
+        </Route>
+        <Route exact path='/details/:id'>
+          <Details />
+        </Route>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 
-    return (
-      <Container fluid>
-        <Col md={{ span: 6, offset: 3 }}>
-          <Card>
-            {routes}
-          </Card>
-        </Col>
-      </Container>
-    )
-  }
+  return (
+    <Container fluid>
+      <Col md={{ span: 6, offset: 3 }}>
+        <Card>
+          {routes}
+        </Card>
+      </Col>
+    </Container>
+  )
 }
 
 export default App
